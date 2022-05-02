cf7974f4b232bd84d7ac15494ee9bf7f623dc60b
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-05-02 18:26:11 -0400

    tables are populating, models are built

--- models/index.js
+++ models/index.js
@@ -1,16 +1,35 @@
-// import models
+// import models - done 
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category -done 
+Product.belongsTo(
+  Category, {
+    foreignKey: "category_id", 
+    onDelete: "CASCADE", 
+  }
+)
 
-// Categories have many Products
+// Categories have many Products - done
+Category.hasMany(
+  Product, {
+    foreignKey: "category_id",
+  }
+)
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) - done
+Product.belongsToMany( Tag, {
+  through: ProductTag, 
+  //as: 
+}); 
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag) -done
+Tag.belongsToMany( Product, {
+  through: ProductTag, 
+ //as
+}); 
 
 module.exports = {
   Product,
